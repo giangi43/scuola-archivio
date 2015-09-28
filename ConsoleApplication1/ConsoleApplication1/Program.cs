c17fd954da6fcfbafc3b9f08915718332e05f8eb
giangi43 <giangi.mamusa@gmail.com>
2015-09-28 13:06:58 +0200

    comprensibility

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -26,12 +26,15 @@ namespace ConsoleApplication1
 
         //}
 
+
+        // tentative to build a cli
         #region cool
 
         Person[] archivio;
         List<ListNode> index;
         testprg help;
         
+        // main
         static void Main(string[] args)
         {
             Program a= new Program();
@@ -40,7 +43,7 @@ namespace ConsoleApplication1
         }
 
         
-
+        // void for be able to use the variable "archivio", "index", "help" outside from the main .... really ugly 
         void fanculo(Program a)
         {
             
@@ -57,16 +60,21 @@ namespace ConsoleApplication1
 
         }
 
+
+        // cli things
         #region cli
 
         public Person[] getArchivio() { return archivio; }
         public List<ListNode> getIndex() { return index; }        
 
-        public void WriteLine(String s) { Console.WriteLine(s); }
+
+        //public void WriteLine(String s) { Console.WriteLine(s); }
+        
+        // wait for a command ... end when the function command return false
         public void ReadCommand() { Console.WriteLine(""); Console.Write("giangi># "); if (command(Console.ReadLine())) ReadCommand(); }
-        public enum Command : byte { see, erease, help, end }
+        //public enum Command : byte { see, erease, help, end }
 
-
+        // decide which command will be execute ... return false with "end"
         bool command(string s)
         {
             Console.WriteLine("");
@@ -77,18 +85,22 @@ namespace ConsoleApplication1
 
         }
 
+        // see commands
         bool seeCommand(string s)
         {
+                // print all the archive when command is equal to (see -a)
             if (s.Split(' ')[1].Equals("-a"))
             {
                 help.printArchive(getArchivio());
                 return true;
             }
+
+                // show all the overloads of the command "see"  (see help)
             else if (s.Split(' ')[1].Equals("help") || s.Split(' ')[1].Equals("?"))
             {
                 Console.WriteLine("");
                 string[] commands = { "see [] <parameter>", "see [-a]", "see help" };
-                string[] utility = { "let you visualize the list based on <parameter>", "let you visualize the archive", "show on screen all the ..." };
+                string[] utility = { "let you visualize the list based on <parameter>", "let you visualize the archive", "show on screen all the overloads" };
                 String str = String.Format("{0,-19} {1,-10}\n\n", "commands", "utility");
                 for (int index = 0; index < commands.Length; index++)
                     str += String.Format("{0,-19} {1,-10:N0}\n",
@@ -96,12 +108,15 @@ namespace ConsoleApplication1
                 Console.WriteLine(str);
                 return true;
             }
+
+                // print the index of what you serech
             else
                 help.showIndex(s.Substring(4), getIndex());
 
             return true;
         }
 
+        //print all the commands 
         bool helpCommand()
         {
             Console.WriteLine("");
@@ -115,7 +130,7 @@ namespace ConsoleApplication1
             return true;
         }
 
-        // svalvola
+        
 
     }
         #endregion
